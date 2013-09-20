65eeef12f170c8339510af3ee246e558eb6c5240
Justin Buchanan <justbuchanan@gmail.com>
2013-09-19 20:09:39 -0400

    Added html template files and a few model classes

--- main.py
+++ main.py
@@ -14,12 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+import os
+import cgi
+import urllib
 import webapp2
+import jinja2
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'])
+
+
+def chapter_key():
+	return ndb.key('LatePlate', "Alpha Tau")
+
+def lateplate_key():
+	return ndb.key('LatePlateRequest', 'LatePlates')
+
+
+class Brother(ndb.Model):
+	user = ndb.UserProperty()
+	approved = ndb.BooleanProperty()
+	admin = ndb.BooleanProperty()
+
+class LatePlateRequest(ndb.Model):
+	user = ndb.UserProperty()
+	meal = ndb.StringProperty()
+	request_time = ndb.DateTimeProperty()
+
+class RecurringLatePlateRequest(LatePlateRequest):
+	day_of_week = ndb.StringProperty()
+
+class OneoffLatePlateRequest(LatePlateRequest):
+	date = ndb.DateProperty()
+
+
+class LatePlateHandler(webapp2.RequestHandler):
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello worlds!')
+
+	def get(self):
+		user = users.get_current_user()
+
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler),
+	('/request', LatePlateHandler),
+	('/list', LatePlateHandler),
+	('/me', LatePlateHandler)
 ], debug=True)
